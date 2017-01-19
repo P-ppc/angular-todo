f5c31417dcd9ff5d79bf956b16d52517a8e91ab7
P-ppc <ppc-user@foxmail.com>
2017-01-19 16:18:55 +0800

    basis add todo

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,13 @@
-window.APP = angular.module("APP", []);
+window.APP = angular.module("APP", ['ngRoute']);
+var app = window.APP;
+
+app.controller('todoCtrl', function ($scope) {
+    $scope.todoList = [];
+    $scope.addTodo = function (title) {
+        if (title == "") return;
+        $scope.todoList.push({
+            title: title
+        });
+    };
+});
+
