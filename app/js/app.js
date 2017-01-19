7faf766840e7e7544e1e6e372370f44dbfb36385
P-ppc <ppc-user@foxmail.com>
2017-01-19 16:37:53 +0800

    ng-click for toggle select

--- app/js/app.js
+++ app/js/app.js
@@ -3,11 +3,21 @@ var app = window.APP;
 
 app.controller('todoCtrl', function ($scope) {
     $scope.todoList = [];
+    $scope.selectedItemId = null;
+    var id = 0;
     $scope.addTodo = function (title) {
         if (title == "") return;
         $scope.todoList.push({
+            id: id++,
             title: title
         });
     };
+    $scope.selectTodoItem = function (item) {
+        if ($scope.selectedItemId == item.id) {
+            $scope.selectedItemId = null;
+        } else {
+            $scope.selectedItemId = item.id;
+        }
+    }
 });
 
